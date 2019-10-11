048ace995d4ac0868f172c7d6d4f9b39e2f46cbd
Martino Facchin <m.facchin@arduino.cc>
2019-10-11 16:51:22 +0200

    Second exercise

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "mbed.h"
 
 DigitalOut pin(LED1);
+DigitalOut pin2(P0_24);
+Thread th;
+
+void blinker() {
+  while (1) {
+     pin2 = 1;
+     wait(1.0f);
+     pin2 = 0;
+     wait(1.0f);
+  }
+}
 
 int main() {
+   th.start(blinker);
    while (1) {
      pin = 1;
      wait(1.0f);
@@ -10,3 +22,4 @@ int main() {
      wait(1.0f);
    }
 }
+
