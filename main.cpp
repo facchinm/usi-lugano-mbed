663890d1d95953a831bf1bd80e9ecbfa33439e59
Martino Facchin <m.facchin@arduino.cc>
2019-10-11 16:52:13 +0200

    Third exercise

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
+#include "Arduino.h"
 #include "mbed.h"
+#include "USBSerial.h"
 
 DigitalOut pin(LED1);
-DigitalOut pin2(P0_24);
 Thread th;
 
 void blinker() {
   while (1) {
-     pin2 = 1;
-     wait(1.0f);
-     pin2 = 0;
-     wait(1.0f);
+    arduino::digitalWrite(LEDB, HIGH);
+    delay(1000);
+    arduino::digitalWrite(LEDB, LOW);
+    delay(1000);
   }
 }
 
 int main() {
+   arduino::pinMode(LEDB, OUTPUT);
    th.start(blinker);
    while (1) {
      pin = 1;
