1b6833fda0b3683b59d1b86cc0a3ef3432235b07
Martino Facchin <m.facchin@arduino.cc>
2019-10-14 11:13:13 +0200

    I2C scan example

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
-#include "Arduino.h"
 #include "mbed.h"
 #include "USBSerial.h"
+I2C i2c(P0_14, P0_15); //SDA, SCL
+USBSerial pc;
 
-DigitalOut pin(LED1);
-Thread th;
+DigitalOut i2c_pullup(P1_0);
+DigitalOut i2c_vdd_enable(P0_22);
 
-void blinker() {
-  while (1) {
-    arduino::digitalWrite(LEDB, HIGH);
-    delay(1000);
-    arduino::digitalWrite(LEDB, LOW);
-    delay(1000);
+void fixNano33BLE() {
+  CoreDebug->DEMCR = 0;
+  NRF_CLOCK->TRACECONFIG = 0;
+}
+
+int main()
+{
+  fixNano33BLE();
+  i2c_pullup = 1;
+  i2c_vdd_enable = 1;
+  pc.printf("RUN\r\n");
+  for(int i = 0; i < 128 ; i++) {
+    i2c.start();
+    if(i2c.write(i << 1) == 1) pc.printf("0x%x ACK \r\n",i); // Send command string
+    i2c.stop();
   }
 }
-
-int main() {
-   arduino::pinMode(LEDB, OUTPUT);
-   th.start(blinker);
-   while (1) {
-     pin = 1;
-     wait(1.0f);
-     pin = 0;
-     wait(1.0f);
-   }
-}
-
